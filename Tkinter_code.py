ccab0b184eb1072c7cb0f5baa6669185e58efa4b
Rishiraj Singh <st22248@ormiston.school.nz>
2023-05-05 11:28:18 +1200

    Added the basics of my code

---
+++ Tkinter_code.py
@@ -0,0 +1,15 @@
+import ttkbootstrap as ttkb
+
+window = ttkb.Window()
+
+window.title("Julie’s Party Hire")
+
+window.iconbitmap("jph.ico")
+
+window.geometry("800x800")
+
+window.minsize(800, 800)
+window.minsize(800, 800)
+
+
+window.mainloop()
