ebfabde1420f995e6a1f9e93fa9d376806a9b545
Rishiraj Singh <st22248@ormiston.school.nz>
2023-05-16 19:58:05 +1200

    2nd Draft of my code
    My code has now been completed with all necessary functions and I have included a working json file with its working functions

--- Tkinter_code.py
+++ Tkinter_code.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 from PIL import ImageTk, Image
+from tkinter import ttk
 from tkinter import messagebox
 import json
 
-
+#Naming my window as tk
 window = tk.Tk()
 
-
-# The Title of the window 
+# The Title of the window
 window.title("Julie’s Party Hire")
 
 # Addes an Icon as the windows logo
@@ -20,22 +20,19 @@ window.geometry("700x700")
 window.minsize(700, 700)
 window.maxsize(700, 700)
 
-#giving the window an background colour 
+# giving the window a background colour
 window.config(bg="#FFF1DC")
 
-
-
-# Defining Constants 
+# Defining Constants
 Max_num_of_items = 500
 Min_num_of_items = 1
 
 # Define the data structure to store items information
-items_data = []
+items_data = {}
 
 # Create a line and draw it on the canvas
 my_rect = tk.Canvas(window, width=800, height=20)
 my_rect.pack()
-
 
 # Shapping my line and filling it with colour
 rect = my_rect.create_rectangle(800, 0, 0, 20, fill="#004AAD")
@@ -43,37 +40,28 @@ bottom_line = my_rect.create_rectangle(800, 20, 0, 17, fill="#000000")
 
 Jph = tk.Label(window, text=("""Julie’s Party 
 Hire'"""), bg="#FAF4EF", font=("Garamond", 20))
+Jph.place(x=570, y=120)
 
-Jph.place(x=570,y=120)
-
-
-
-
-# Labels and Entry Boxes 
+# Labels and Entry Boxes
 name_label = tk.Label(window, text="Full Name:", background="#FFD18B")
 name_label.pack(anchor='w')
-name_entry = tk.Entry(window, border = 1, relief='solid')
+name_entry = tk.Entry(window, border=1, relief='solid')
 name_entry.pack(anchor='w')
-
 
 receipt_num_label = tk.Label(window, text="Receipt Number:", background="#FFD18B")
 receipt_num_label.pack(anchor='w')
-receipt_entry = tk.Entry(window, border = 1, relief='solid')
+receipt_entry = tk.Entry(window, border=1, relief='solid')
 receipt_entry.pack(anchor='w')
-
 
 item_label = tk.Label(window, text="Item Hired:", background="#FFD18B")
 item_label.pack(anchor='w')
-item_entry = tk.Entry(window, border = 1, relief='solid')
+item_entry = tk.Entry(window, border=1, relief='solid')
 item_entry.pack(anchor='w')
-
-
 
 num_item_label = tk.Label(window, text="Number of items hired:", background="#FFD18B")
 num_item_label.pack(anchor='w')
-num_item_entry = tk.Entry(window, border = 1, relief='solid')
+num_item_entry = tk.Entry(window, border=1, relief='solid')
 num_item_entry.pack(anchor='w')
-
 
 # Define functions for adding, updating, and deleting camp information
 def submit_data():
@@ -84,7 +72,6 @@ def submit_data():
     receipt = receipt_entry.get().strip()
     item = item_entry.get().strip()
     num_item = num_item_entry.get().strip()
-    
 
     # Check if any input fields are empty
     if not all([name, receipt, item, num_item]):
@@ -93,18 +80,25 @@ def submit_data():
 
     # Validate the input
     try:
-        receipt = int(receipt) 
+        receipt = int(receipt)
         num_item = int(num_item)
     except ValueError:
         error_label.config(text="Please Enter Valid Information!!. (Only Numbers are valid )")
         return
 
-    if num_item  < Min_num_of_items or num_item > Max_num_of_items:
+    if num_item < Min_num_of_items or num_item > Max_num_of_items:
         error_label.config(text=f"Number of items hired must be between {Min_num_of_items} and {Max_num_of_items}")
         return
 
-    # Add the information to the data structure
-    items_data.append([name, receipt, item, num_item])
+    with open("data.json", "r") as file:
+        customer_data = json.load(file)
+        customer_data["Full Name:"].append(name)
+        customer_data["Receipt Number:"].append(receipt)
+        customer_data["Item hired:"].append(item)
+        customer_data["Number of Items Hired:"].append(num_item)
+
+    with open("data.json", "w") as file:
+        json.dump(customer_data, file)
 
     # Clear the input fields
     name_entry.delete(0, tk.END)
@@ -112,114 +106,106 @@ def submit_data():
     item_entry.delete(0, tk.END)
     num_item_entry.delete(0, tk.END)
 
-    with open("data.json", "w") as file:
-        customer_data = json.load(file)
-        customer_data['Customer Name'].append(name)
-        customer_data['Receipt'].append(receipt)
-        customer_data['Item hired'].append(item)
-        customer_data['Item hired amount'].append(num_item)
-
-    with open('data.json', 'w') as file:
-        json.dump(customer_data, file)
-
-
-
-
     # Update the display
     update_display()
 
 
-
-#Set Display function
-
+# Set Display function
 def update_display():
-   
-    listbox.delete(0, tk.END)
-    print(items_data)
+    with open('data.json', 'r') as file:
+        data = json.load(file)
 
+    trv.delete(*trv.get_children())  # Clear the Treeview
 
-    for name, receipt, item, num_item in items_data:
-        listbox.insert(
-            tk.END,
-            f"Full Name: {name}   Receipt Number: {receipt}    Item hired: {item}    Number of Items Hired: {num_item}"
-            )
+    for i in range(len(data['Full Name:'])):
+        trv.insert("", "end", values=(
+            i + 1,
+            data['Full Name:'][i],
+            data['Receipt Number:'][i],
+            data['Item hired:'][i],
+            data['Number of Items Hired:'][i]
+        ))
 
-
-
-#Set Update function
 
 def update_data():
-    selected_item = listbox.curselection()
-
-
-    for i in listbox.curselection():
-        e = listbox.get(i).split(" ")
+    selected_item = trv.focus()
 
     if not selected_item:
-        error_label.config("Please select an item to update")
+        error_label.config(text="Please select an item to update")
         return
-    
-        
-    userData = {
-        "Name": e[1],
-        "Receipt": int(e[8].replace("(","")),
-        "Item hired": e[14],
-        "Number of items hired": int(e[22].replace("(",""))
-        }
-    listbox.delete(0, tk.END)
 
-    temp_list = []
-    [temp_list.append(i) for i in userData.values()]
+    item_data = trv.item(selected_item)['values']
 
-    for i in items_data:
-        if temp_list == i:
-            print(i)
-            print("EEE")
-            items_data.remove(i)
+    name_entry.delete(0, tk.END)
+    receipt_entry.delete(0, tk.END)
+    item_entry.delete(0, tk.END)
+    num_item_entry.delete(0, tk.END)
 
-    print(items_data)
-    print(userData)
+    name_entry.insert(0, item_data[1])
+    receipt_entry.insert(0, item_data[2])
+    item_entry.insert(0, item_data[3])
+    num_item_entry.insert(0, item_data[4])
 
-    name_entry.insert(0, str(userData["Full Name"]))
-    receipt_entry.insert(0, str(userData["Receipt Number"]))
-    item_entry.insert(0, str(userData["Items hired"]))
-    num_item_entry.insert(0, str(userData["Number of items hired"]))
-
-
-#Set Delete function
 
 def delete_row():
-    selected_item = listbox.curselection()
+    selected_item = trv.focus()
 
     if not selected_item:
-        error_label.config(text=f"Please selecte a row to delete")
+        error_label.config(text="Please select a row to delete")
         return
-    index = selected_item[0]
+
+    warning = messagebox.askquestion('Warning', 'Do you want to continue? (If you proceed, all data will be lost)')
+
+    if warning == "yes":
+        trv.delete(selected_item)
 
 
-    del items_data[index]
-    return
+trv = ttk.Treeview(window, columns=(1, 2, 3, 4, 5), show="headings", height=50)
+trv.pack(anchor="w",ipadx=400)
+
+trv.heading(1, text="ID", anchor="center")
+trv.heading(2, text="Full Name", anchor="center")
+trv.heading(3, text="Receipt Number", anchor="center")
+trv.heading(4, text="Item Hired", anchor="center")
+trv.heading(5, text="Number of Items Hired", anchor="center")
+
+trv.column("#1", anchor="w", width=40, stretch=True)
+trv.column("#2", anchor="w", width=150, stretch=False)
+trv.column("#3", anchor="w", width=150, stretch=False)
+trv.column("#4", anchor="w", width=150, stretch=False)
+trv.column("#5", anchor="w", width=150, stretch=False)
+
+
+with open('data.json', 'r') as file:
+    data = json.load(file)
+
+trv.delete(*trv.get_children())  # Clear the Treeview
+
+for i in range(len(data['Full Name:'])):
+    trv.insert("", "end", values=(
+        i + 1,
+        data['Full Name:'][i],
+        data['Receipt Number:'][i],
+        data['Item hired:'][i],
+        data['Number of Items Hired:'][i]
+    ))
 
 
 
-# Create the listbox
-listbox = tk.Listbox(window)
-listbox.pack(anchor="w", ipadx=400)
+# Buttons
+# Submit
+submit_button = tk.Button(window, text="Submit", bg='green', command=submit_data)
+submit_button.place(x=160, y=158)
 
+# Update
+update_button = tk.Button(window, text="Update", bg='yellow', command=update_data)
+update_button.place(x=250,y=158)
 
-
-#Buttons
-#Submit
-submit_button = tk.Button(window, text= "Submit", bg='green', command=submit_data)
-submit_button.place(x=160,y=160)
-
-#Update
-update_button = tk.Button(window, text= "Update", bg='yellow', command=update_data)
-update_button.pack(anchor='w')
-
-#Delete 
+#Delete
 delete_button = tk.Button(window, text= "Delete Row", bg='orange', command=delete_row)
 delete_button.place(x=630,y=350)
+
+
 
 
 
@@ -235,7 +221,7 @@ def msg1():
 #Exit Program 
 
 exit_button = tk.Button(window, text="Exit Program", bg='red', command=msg1)
-exit_button.pack(anchor='e')
+exit_button.place(x=630,y=400)
 
 
 img = Image.open("Logo.jpg")
@@ -264,13 +250,7 @@ img_label.place(x=595,y=23)
 error_label = tk.Label(window, fg="red")
 error_label.pack(anchor="w")
 
-# Create a line and draw it on the canvas
-my_rect_2 = tk.Canvas(window, width=700, height=600)
-my_rect_2.place(x=0,y=450)
-
-# Shapping my line and filling it with colour
-rect_2 = my_rect_2.create_rectangle(800, 800, 0, 450, fill="#004AAD")
-upper_line = my_rect_2.create_rectangle(700, 440, 0, 450, fill="black")
 
 
-window.mainloop()
+
+window.mainloop() 
